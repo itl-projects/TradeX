6758d938102ee1cb294fe513d4365ed8e7ad17bf
Gurjeetsingh85 <60958940+Gurjeetsingh85@users.noreply.github.com>
2020-04-11 16:09:27 +0530

    Fix

--- app/src/main/java/com/example/tradex/TradeXLoginPage.java
+++ app/src/main/java/com/example/tradex/TradeXLoginPage.java
@@ -2,7 +2,9 @@ package com.example.tradex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TradeXLoginPage extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class TradeXLoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trade_x_login_page);
+    }
+
+    public void signUpButton(View view) {
+        Intent intent=new Intent(TradeXLoginPage.this,Home.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void loginButton(View view) {
+        Intent intent=new Intent(TradeXLoginPage.this,Home.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void facebook(View view) {
+        Intent intent=new Intent(TradeXLoginPage.this,Home.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void google(View view) {
+        Intent intent=new Intent(TradeXLoginPage.this,Home.class);
+        startActivity(intent);
+        finish();
     }
 }
