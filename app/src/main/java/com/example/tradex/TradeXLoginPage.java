4b7ffa325ead59bc54a3774a525f940d6e2d9eec
Gurjeetsingh85 <60958940+Gurjeetsingh85@users.noreply.github.com>
2020-04-11 14:13:42 +0530

    Add Login Page

---
+++ app/src/main/java/com/example/tradex/TradeXLoginPage.java
@@ -0,0 +1,14 @@
+package com.example.tradex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TradeXLoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trade_x_login_page);
+    }
+}
